fddc611704deed9548e2784f4b4deebdcbd4a47c
DanielRasho <ray22933@uvg.edu.gt>
2023-10-17 09:22:51 -0600

    restart project

--- app/src/main/java/edu/uvg/ejemplofirebaseuvg/EmployeeModel.kt
+++
@@ -1,8 +0,0 @@
-package edu.uvg.ejemplofirebaseuvg
-
-data class EmployeeModel(
-    var empId: String? = null,
-    var empName: String? = null,
-    var empAge: String? = null,
-    var empSalary: String? = null
-)
